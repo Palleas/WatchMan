3f32dae14b41e4f04c27598b6005775150df5ac9
Romain Pouclet <rpouclet@mirego.com>
2015-09-30 09:55:01 -0400

    Initial Commit

---
+++ WatchMan/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WatchMan
+//
+//  Created by Romain Pouclet on 2015-09-30.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
