199d99c64195bbbbb3e5e62d22a56abd05456254
Romain Pouclet <romain.pouclet@gmail.com>
2015-10-05 08:29:09 -0400

    Wrapping directory watching in SignalProducer

--- WatchMan/Watcher.swift
+++ WatchMan/Watcher.swift
@@ -10,6 +10,14 @@ import Foundation
 import ReactiveCocoa
 import CoreServices
 
+class Wrapper<T>: AnyObject {
+    internal let value: T
+
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
 func watchFolder(path: String) -> SignalProducer<String, NoError> {
     return SignalProducer { sink, disposable in
         let flags = UInt32(kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagFileEvents)
@@ -17,10 +25,15 @@ func watchFolder(path: String) -> SignalProducer<String, NoError> {
         
         let eventCallback: FSEventStreamCallback = { (stream, contextInfo, numEvents, eventPaths, eventFlags, eventIds) in
             print("***** FSEventCallback Fired *****")
+            
+            let yolo = Unmanaged<Wrapper<Signal<String, NoError>.Observer>>.fromOpaque(COpaquePointer(contextInfo)).takeUnretainedValue()
+            sendNext(yolo.value, "")
         }
 
-        var name: String = "romain"
-        var context = FSEventStreamContext(version: 0, info: &name, retain: nil, release: nil, copyDescription: nil)
+        let unmanaged = Unmanaged.passRetained(Wrapper(sink))
+        let pointer: UnsafeMutablePointer<Void> = UnsafeMutablePointer(unmanaged.toOpaque())
+
+        var context = FSEventStreamContext(version: 0, info: pointer, retain: nil, release: nil, copyDescription: nil)
         let eventStream = FSEventStreamCreate(kCFAllocatorDefault, eventCallback, &context, [path], sinceWhen, 3.0, flags)
         
         FSEventStreamScheduleWithRunLoop(eventStream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)
